fc8e0b55cbed95f2b9b8ada26ba2f9730c997798
Victor Hugo <victormuza133@gmail.com>
2024-04-14 16:21:14 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+import addRoutes from "./routes.js"
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import express from 'express'
+import sgbd from './sgbd.js'
+
+sgbd.init()
+
+const app = express()
+app.use(bodyParser.json())       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}))
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('🚒 Hello World CRUD')
+})
+
+addRoutes(app)
+
+app.listen(3000, () => {
+    console.log('🔥 estou escutando na porta 3000');
+})
